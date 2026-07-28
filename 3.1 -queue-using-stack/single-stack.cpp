da47b482ac079ba4709e76c23bf51d1883bc27c7
renu-lasya <lasyarenu2007reddy@gmail.com>
2026-07-28 23:19:36 +0530

    Create single-stack.cpp

---
+++ 3.1 -queue-using-stack/single-stack.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class MyQueue
+{
+    stack<int> s;
+
+public:
+
+    void push(int x)
+    {
+        s.push(x);
+        cout<<"Element Inserted Successfully!"<<endl;
+    }
+
+    int pop()
+    {
+        if(s.empty())
+        {
+            cout<<"Queue is Empty!"<<endl;
+            return -1;
+        }
+
+        int x=s.top();
+        s.pop();
+
+        if(s.empty())
+            return x;
+
+        int item=pop();
+        s.push(x);
+
+        return item;
+    }
+
+    int front()
+    {
+        if(s.empty())
+        {
+            cout<<"Queue is Empty!"<<endl;
+            return -1;
+        }
+
+        int x=s.top();
+        s.pop();
+
+        if(s.empty())
+        {
+            s.push(x);
+            return x;
+        }
+
+        int item=front();
+        s.push(x);
+
+        return item;
+    }
+
+    bool empty()
+    {
+        return s.empty();
+    }
+};
+
+int main()
+{
+    MyQueue q;
+    int choice,x;
+
+   do
+{
+    cout << "\n1. Push" << endl;
+    cout << "2. Pop" << endl;
+    cout << "3. Front" << endl;
+    cout << "4. Empty" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Enter Choice: ";   // <-- Add this line
+
+    cin >> choice;
+
+    switch(choice)
+    {
+        case 1:
+            cout << "Enter Element: ";
+            cin >> x;
+            q.push(x);
+            break;
+
+        case 2:
+            x = q.pop();
+            if(x != -1)
+                cout << "Deleted Element: " << x << endl;
+            break;
+
+        case 3:
+            x = q.front();
+            if(x != -1)
+                cout << "Front Element: " << x << endl;
+            break;
+
+        case 4:
+            if(q.empty())
+                cout << "Queue is Empty" << endl;
+            else
+                cout << "Queue is Not Empty" << endl;
+            break;
+
+        case 5:
+            cout << "Program Ended" << endl;
+            break;
+
+        default:
+            cout << "Invalid Choice!" << endl;
+    }
+
+} while(choice != 5);
+}
