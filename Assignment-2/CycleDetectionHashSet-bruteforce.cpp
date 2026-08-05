f190ad34633fa824d5bf6ee6514ee14518215aa7
renu-lasya <lasyarenu2007reddy@gmail.com>
2026-08-05 15:23:57 +0530

    Update CycleDetectionHashSet-bruteforce.cpp

--- Assignment-2/CycleDetectionHashSet-bruteforce.cpp
+++ Assignment-2/CycleDetectionHashSet-bruteforce.cpp
@@ -1,21 +1,49 @@
+#include <iostream>
 #include <unordered_set>
 using namespace std;
 
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
+struct Node
+{
+    int data;
+    Node* next;
 
-        unordered_set<ListNode*> visited;
-
-        while(head != NULL)
-        {
-            if(visited.count(head))
-                return true;
-
-            visited.insert(head);
-            head = head->next;
-        }
-
-        return false;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
     }
 };
+
+bool hasCycle(Node* head)
+{
+    unordered_set<Node*> visited;
+
+    while(head != NULL)
+    {
+        if(visited.count(head))
+            return true;
+
+        visited.insert(head);
+        head = head->next;
+    }
+
+    return false;
+}
+
+int main()
+{
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+
+    
+    head->next->next->next->next = head->next;
+
+    if(hasCycle(head))
+        cout<<"Cycle Found";
+    else
+        cout<<"No Cycle";
+
+    return 0;
+}
