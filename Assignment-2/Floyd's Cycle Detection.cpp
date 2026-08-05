3f4445b4160fe2a58cae3948cf4ddc89fa6c94cb
renu-lasya <lasyarenu2007reddy@gmail.com>
2026-08-05 15:24:54 +0530

    Update Floyd's Cycle Detection.cpp

--- Assignment-2/Floyd's Cycle Detection.cpp
+++ Assignment-2/Floyd's Cycle Detection.cpp
@@ -1,24 +1,49 @@
-#include <unordered_set>
+#include <iostream>
 using namespace std;
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
 
-        if(head == NULL)
-            return false;
+struct Node
+{
+    int data;
+    Node* next;
 
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while(fast != NULL && fast->next != NULL)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if(slow == fast)
-                return true;
-        }
-
-        return false;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
     }
 };
+
+bool hasCycle(Node* head)
+{
+    Node *slow=head;
+    Node *fast=head;
+
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(slow==fast)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    Node* head=new Node(1);
+    head->next=new Node(2);
+    head->next->next=new Node(3);
+    head->next->next->next=new Node(4);
+
+    // Create cycle
+    head->next->next->next->next=head->next;
+
+    if(hasCycle(head))
+        cout<<"Cycle Found";
+    else
+        cout<<"No Cycle";
+
+    return 0;
+}
