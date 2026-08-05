641a5f0fb1574f11372c7ba3ae890e16e231e1c2
renu-lasya <lasyarenu2007reddy@gmail.com>
2026-08-05 14:56:39 +0530

    Create Floyd's Cycle Detection.cpp

---
+++ Assignment-2/Floyd's Cycle Detection.cpp
@@ -0,0 +1,24 @@
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        if(head == NULL)
+            return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
